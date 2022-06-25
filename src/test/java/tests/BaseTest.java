92b158ed2ffea9461f39ed1aed6067c5874c4473
Oleg_Zaharenko <zakharenka.oleg@gmail.com>
2022-06-25 16:24:27 +0300

    a

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,9 +2,11 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 
-import com.codeborne.selenide.logevents.SelenideLogger;
+import com.codeborne.selenide.logevents.SelenideLogger;;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest {
 
@@ -12,8 +14,15 @@ public class BaseTest {
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.browserSize = "1920x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+
     }
 }
