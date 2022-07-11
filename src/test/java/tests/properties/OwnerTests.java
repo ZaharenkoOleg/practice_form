c31b513f732421a19a8530c98887cc168e171186
Oleg_Zaharenko <zakharenka.oleg@gmail.com>
2022-07-11 16:12:20 +0300

    a

--- src/test/java/tests/properties/OwnerTests.java
+++ src/test/java/tests/properties/OwnerTests.java
@@ -2,12 +2,14 @@ package tests.properties;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class OwnerTests {
     CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     @Test
+    @Tag("owner")
     void someTest() {
         String login = config.login();
         String password = config.password();
