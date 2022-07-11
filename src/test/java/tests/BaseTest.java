306647d26d5c445ca6c90bfdaa5cb53b60797f8b
Oleg_Zaharenko <zakharenka.oleg@gmail.com>
2022-07-11 19:17:37 +0300

    a

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -30,8 +30,8 @@ public class BaseTest {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("version", "100");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        String remoteBrowser = System.getProperty("remote", "google.com");
-        Configuration.remote = String.format("https://%s:%s@%s",  config.login(), config.password(), remoteBrowser);
+        String remoteURL = System.getProperty("remoteURL");
+        Configuration.remote = String.format("https://%s:%s@%s", config.login(), config.password(), remoteURL );
     }
         @AfterEach
         void afterEach() {
